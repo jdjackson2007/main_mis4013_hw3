d17f6cf4777afcd92934b8e8233235b747fbeadf
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-10-16 13:29:29 -0500

    Create model-category.php

---
+++ model-category.php
@@ -0,0 +1,16 @@
+<?php
+function selectCategory() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Category_ID,Category_Name,Category_Description,Category_IsActive FROM `mis-4013_hw3`.category_table");
+        $stmt->bind_param();
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
