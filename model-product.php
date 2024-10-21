63fce5bbc4ed8aee40a02c89ca998c492d1f211c
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-10-21 13:48:31 -0500

    Update model-product.php

--- model-product.php
+++ model-product.php
@@ -2,7 +2,7 @@
 function selectCategory() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("Product_ID, Category_ID, VendorProduct_ID, Product_Name varchar, Product_Description, Product_Quanity, Product_Price, `mis-4013_hw3`.product_table");
+        $stmt = $conn->prepare("SELECT Product_ID,Category_ID,VendorProduct_ID,Product_Name,Product_Description,Product_Quanity,Product_Price, FROM `mis-4013_hw3`.product_table");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
