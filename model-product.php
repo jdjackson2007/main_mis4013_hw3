f17a6f08a971a04cd783591c2170c6e6ef17131e
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-10-21 20:10:08 -0500

    Update model-product.php

--- model-product.php
+++ model-product.php
@@ -1,5 +1,5 @@
 <?php
-function selectCategory() {
+function selectproduct() {
     try {
         $conn = get_db_product();
         $stmt = $conn->prepare("SELECT Product_ID,Category_ID,VendorProduct_ID,Product_Name,Product_Description,Product_Quanity,Product_Price, FROM `mis-4013_hw3`.product_table");
