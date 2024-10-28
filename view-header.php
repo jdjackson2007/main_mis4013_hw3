f5c60953559c3e440a66a2522e688e3a479d32e4
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-10-28 13:23:54 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -31,17 +31,6 @@
       </ul>
     </div>
   </div>
-  <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-  <label for="floatingInput">Email address</label>
-</div>
-<div class="form-floating mb-3">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
-</div>
-<button class="btn btn-primary w-100" onclick="login()">Log In</button>
-<div id="loginMessage" class="mt-3"></div>
-</div>
         <div class="form-floating mb-3">
   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
   <label for="floatingInput">Email address</label>
