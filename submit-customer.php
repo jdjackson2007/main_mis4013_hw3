d471883c7718109092378911cfd52e0f203a5e90
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-06 19:49:06 -0600

    Update submit-customer.php

--- submit-customer.php
+++ submit-customer.php
@@ -4,15 +4,19 @@ require_once("util-db.php");
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+
+    $customerid = $_POST['inputCustomerID']
+    $customerfirstname = $_POST['inputCustomerFirstName']
+    $customerlastname = $_POST['inputCustomerLastName']
     $email = $_POST['inputEmail4'];
-    $password = $_POST['inputPassword4'];
-    $reenterPassword = $_POST['reenterinputPassword4'];
+    $phone = $_POST['inputPhone']
     $address = $_POST['inputAddress'];
     $address2 = $_POST['inputAddress2'];
     $city = $_POST['inputCity'];
     $state = $_POST['inputState'];
     $zip = $_POST['inputZip'];
+    $password = $_POST['inputPassword4'];
+    $reenterPassword = $_POST['reenterinputPassword4'];
     
     
     if ($password !== $reenterPassword) {
@@ -21,11 +25,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
    
-    $conn = get_db_connection();
+    $customers = get_db_connection();
     
-    if ($conn) {
+    if ($customers) {
        
-        $stmt = $conn->prepare("INSERT INTO customers (email, password, address, address2, city, state, zip) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $customers = selectCustomer();->prepare("SELECT Customer_ID, Customer_FirstName, Customer_LastName,Customer_Email, Customer_Phone,Customer_Address FROM `mis-4013_hw3`.customer_table VALUES (?, ?, ?, ?, ?, ?, ?)");
         
        
         $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
