0c6493eb2ba1a53b0d59194685349a6ff041d197
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-06 20:10:26 -0600

    Update submit-customer.php

--- submit-customer.php
+++ submit-customer.php
@@ -25,11 +25,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
    
-    $customers = get_db_connection();
+    $conn = get_db_connection();
     
-    if ($customers) {
-       
-        $customers = selectCustomer();->prepare("SELECT Customer_ID, Customer_FirstName, Customer_LastName,Customer_Email, Customer_Phone,Customer_Address FROM `mis-4013_hw3`.customer_table VALUES (?, ?, ?, ?, ?, ?, ?)");
+    if ($conn) {
+        
+        $stmt = $conn->prepare();->prepare("SELECT Customer_ID, Customer_FirstName, Customer_LastName,Customer_Email, Customer_Phone,Customer_Address FROM `mis-4013_hw3`.customer_table VALUES (?, ?, ?, ?, ?, ?, ?)");
         
        
         $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
