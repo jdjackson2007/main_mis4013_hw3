9e27c67c82954654d5d5386ac02147f0d516e192
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-07 19:51:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,12 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>HomeWork3</h1>
-<a href="category.php">CATEGORY</a>
-<a href="product.php">PRODUCT</a>
-<a href="employee.php">EMPLOYEE</a>
+    <a href="category.php">CATEGORY</a>
+    <a href="product.php">PRODUCT</a>
+    <a href="employee.php">EMPLOYEE</a>
+    
+    
+    <script src="js/script.js"></script>
            
 <?php
 include "view-footer.php";
