cb728b4b8551fa7738cdbff8b5804784a010a971
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-07 21:48:05 -0600

    Update customer-login.php

--- customer-login.php
+++ customer-login.php
@@ -1,7 +1,3 @@
-<?php
-include "create-customer-login.php";
-include "customer-login.php";
-?>
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -19,24 +15,12 @@ include "customer-login.php";
                         <label for="loginPassword" class="form-label">Password</label>
                         <input type="password" class="form-control" id="loginPassword" name="loginPassword" required>
                     </div>
-                   <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="loginModalLabel">Log In</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="login.php" method="POST" class="row g-3">
-                    <!-- Form fields for login (e.g., Email and Password) -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Log In</button>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
