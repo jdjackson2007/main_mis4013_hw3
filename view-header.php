3e5d30c9530755871a03e3ac041e2fad3b27eb1a
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-07 20:57:01 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,10 +6,10 @@
     <title><?php echo $pageTitle;?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  
   <body>
-   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <div Class:="Container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
