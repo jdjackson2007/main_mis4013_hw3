2d531af13477347a6be6d4207e171a00b0b97a8a
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-07 21:09:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,13 @@ include "view-header.php";
     <a href="category.php">CATEGORY</a>
     <a href="product.php">PRODUCT</a>
     <a href="employee.php">EMPLOYEE</a>
-    
-    
     <script src="js/script.js"></script>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">
+   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">
     Log In
 </button>
-<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" data-bs-target="#createaccountModal">
+<button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#createAccountModal">
     Create Account
-</button>
-           
+</button>           
 <?php
 include "view-footer.php";
 ?>
