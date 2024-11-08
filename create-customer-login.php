e114b3350cc3c6605ccdf67cd5641010ccb55313
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-07 21:46:46 -0600

    Update create-customer-login.php

--- create-customer-login.php
+++ create-customer-login.php
@@ -1,57 +1,4 @@
-<?php
-include "create-customer-login.php";
-include "customer-login.php";
-?>
-<form action="submit-customer.php" method="POST" class="row g-3">
-    <div class="col-md-6">
-        <label for="inputCustomerFirstName" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="inputCustomerFirstName" name="inputCustomerFirstName" required>
-    </div>
-    <div class="col-md-6">
-        <label for="inputCustomerLastName" class="form-label">Last Name</label>
-        <input type="text" class="form-control" id="inputCustomerLastName" name="inputCustomerLastName" required>
-    </div>
-    <div class="col-md-6">
-        <label for="inputEmail4" class="form-label">Email</label>
-        <input type="email" class="form-control" id="inputEmail4" name="inputEmail4" required>
-    </div>
-    <div class="col-md-6">
-        <label for="inputPhone" class="form-label">Phone</label>
-        <input type="text" class="form-control" id="inputPhone" name="inputPhone" required>
-    </div>
-    <div class="col-12">
-        <label for="inputAddress" class="form-label">Address</label>
-        <input type="text" class="form-control" id="inputAddress" name="inputAddress" required>
-    </div>
-    <div class="col-12">
-        <label for="inputAddress2" class="form-label">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" name="inputAddress2">
-    </div>
-    <div class="col-md-6">
-        <label for="inputCity" class="form-label">City</label>
-        <input type="text" class="form-control" id="inputCity" name="inputCity" required>
-    </div>
-    <div class="col-md-4">
-        <label for="inputState" class="form-label">State</label>
-        <select id="inputState" name="inputState" class="form-select" required>
-            <option selected>Choose...</option>
-            <option value="NY">New York</option>
-            <!-- Add other options here -->
-        </select>
-    </div>
-    <div class="col-md-2">
-        <label for="inputZip" class="form-label">Zip</label>
-        <input type="text" class="form-control" id="inputZip" name="inputZip" required>
-    </div>
-    <div class="col-md-6">
-        <label for="inputPassword4" class="form-label">Password</label>
-        <input type="password" class="form-control" id="inputPassword4" name="inputPassword4" required>
-    </div>
-    <div class="col-md-6">
-        <label for="reenterinputPassword4" class="form-label">Re-enter Password</label>
-        <input type="password" class="form-control" id="reenterinputPassword4" name="reenterinputPassword4" required>
-    </div>
-   <div class="modal fade" id="createAccountModal" tabindex="-1" aria-labelledby="createAccountModalLabel" aria-hidden="true">
+<div class="modal fade" id="createAccountModal" tabindex="-1" aria-labelledby="createAccountModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -60,10 +7,60 @@ include "customer-login.php";
             </div>
             <div class="modal-body">
                 <form action="submit-customer.php" method="POST" class="row g-3">
-                    <!-- Form fields go here (e.g., First Name, Email, etc.) -->
+                    <div class="col-md-6">
+                        <label for="inputCustomerFirstName" class="form-label">First Name</label>
+                        <input type="text" class="form-control" id="inputCustomerFirstName" name="inputCustomerFirstName" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputCustomerLastName" class="form-label">Last Name</label>
+                        <input type="text" class="form-control" id="inputCustomerLastName" name="inputCustomerLastName" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputEmail4" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="inputEmail4" name="inputEmail4" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputPhone" class="form-label">Phone</label>
+                        <input type="text" class="form-control" id="inputPhone" name="inputPhone" required>
+                    </div>
+                    <div class="col-12">
+                        <label for="inputAddress" class="form-label">Address</label>
+                        <input type="text" class="form-control" id="inputAddress" name="inputAddress" required>
+                    </div>
+                    <div class="col-12">
+                        <label for="inputAddress2" class="form-label">Address 2</label>
+                        <input type="text" class="form-control" id="inputAddress2" name="inputAddress2">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputCity" class="form-label">City</label>
+                        <input type="text" class="form-control" id="inputCity" name="inputCity" required>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="inputState" class="form-label">State</label>
+                        <select id="inputState" name="inputState" class="form-select" required>
+                            <option selected>Choose...</option>
+                            <option value="NY">New York</option>
+                            <!-- Add other options here -->
+                        </select>
+                    </div>
+                    <div class="col-md-2">
+                        <label for="inputZip" class="form-label">Zip</label>
+                        <input type="text" class="form-control" id="inputZip" name="inputZip" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputPassword4" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4" name="inputPassword4" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="reenterinputPassword4" class="form-label">Re-enter Password</label>
+                        <input type="password" class="form-control" id="reenterinputPassword4" name="reenterinputPassword4" required>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Create Account</button>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
-</form>
