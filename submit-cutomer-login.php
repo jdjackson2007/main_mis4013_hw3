f3f7c669cdb933bc3eb6f68621d8f3d26808c92f
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-18 09:52:51 -0600

    Update submit-cutomer-login.php

--- submit-cutomer-login.php
+++ submit-cutomer-login.php
@@ -53,3 +53,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Invalid request method.";
 }
 ?>
+<?php
+function insertCustomer() {
+    try {
+        $conn = get_db_connection($CustFirst,$CustLast,$CustEmail,$CustPhone,$CustAddress,$CustAddress2,$CustCity,$CustState,$CustZip,$CustPass);
+        $stmt = $conn->prepare("INSERT INTO `mis-4013_hw3`.customer_table' ('Customer_FirstName','Customer_LastName','Customer_Email','Customer_Phone','Customer_Address','Customer_Address2','Customer_City','Customer_State','Customer_Zip','Customer_Password') Values(?,?,?,?,?,?,?,?,?,?)");
+        $stmt->bind_param("ssssssssss", $CustFirst,$CustLast,$CustEmail,$CustPhone,$CustAddress,$CustAddress2,$CustCity,$CustState,$CustZip,$CustPass);
+        $Success = $stmt->execute();
+        $conn->close();
+        return $Success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
