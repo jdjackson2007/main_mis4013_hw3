ec8124b66b8ba823c0a4ab96765e685b2d711c62
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-18 14:24:00 -0600

    Update model-customer.php

--- model-customer.php
+++ model-customer.php
@@ -14,16 +14,16 @@ function selectCustomer() {
 }
 ?>
 <?php
-function insertCustomer($CustFirst, $CustLast, $CustEmail, $CustPhone, $CustAddress, $CustAddress2, $CustCity, $CustState, $CustZip, $CustPass) {
+function insertCustomer($CustID, $CustFirst, $CustLast, $CustEmail, $CustPhone, $CustAddress, $CustAddress2, $CustCity, $CustState, $CustZip, $CustPass) {
     try {
         $conn = get_db_connection(); 
-        $stmt = $conn->prepare("INSERT INTO `mis-4013_hw3`.customer_table 
-            (`Customer_FirstName`, `Customer_LastName`, `Customer_Email`, `Customer_Phone`, 
-            `Customer_Address`, `Customer_Address2`, `Customer_City`, `Customer_State`, 
-            `Customer_Zip`, `Customer_Password`) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO `mis-4013_hw3`.customer_table
+(`Customer_ID`,`Customer_FirstName`, `Customer_LastName`, `Customer_Email`, `Customer_Phone`,
+ `Customer_Address`, `Customer_Address2`, `Customer_City`, `Customer_State`, `Customer_Zip`, `Customer_Password`)) 
+            VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->bind_param(
-            "ssssssssss", 
+            "sssssssssss", 
+            $CustID
             $CustFirst, 
             $CustLast, 
             $CustEmail, 
