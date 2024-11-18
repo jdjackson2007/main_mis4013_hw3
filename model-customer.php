e3ecbd83d8d18c09e02c8b493ffa0c2264e857af
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-18 09:46:05 -0600

    Update model-customer.php

--- model-customer.php
+++ model-customer.php
@@ -13,18 +13,4 @@ function selectCustomer() {
     }
 }
 ?>
-<?php
-function insertCustomer() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `mis-4013_hw3`.customer_table' ('Customer_ID','Customer_FirstName','Customer_LastName','Customer_Email','Customer_Phone','Customer_Address','Customer_Address2','Customer_City','Customer_State','Customer_Zip','Customer_Password') Values(?,?,?,?,?,?,?,?,?,?,?)");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-?>
+
