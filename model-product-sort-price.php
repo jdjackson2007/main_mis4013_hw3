f230a69596ebfde68e28f2571ee040871328cee2
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-20 13:34:12 -0600

    Update model-product-sort-price.php

--- model-product-sort-price.php
+++ model-product-sort-price.php
@@ -1,9 +1,8 @@
 <?php
-function selectProduct($pprice) {
+function selectProduct() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Product_ID,Category_ID,Product_Name,Product_Description,Product_Price FROM `mis-4013_hw3`.product_table WHERE Product_Price=?");
-         $stmt->bind_param("d", $pprice);
+        $stmt = $conn->prepare("SELECT Product_ID,Category_ID,Product_Name,Product_Description,Product_Price FROM `mis-4013_hw3`.product_table");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
