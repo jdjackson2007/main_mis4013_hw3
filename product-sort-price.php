2b882c0ee40dd056945466189c1f6733f8487448
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-20 13:39:00 -0600

    Update product-sort-price.php

--- product-sort-price.php
+++ product-sort-price.php
@@ -1,7 +1,7 @@
 <?php
 Require_Once("util-db.php");
   Require_Once("model-productsortprice.php");
-$pageTitle = "PRODUCT By Price";
+$pageTitle = "PRODUCTS GROUPED BY PRICE";
 include "view-header.php";
 $products = selectProduct();
 include "view-product.php";
