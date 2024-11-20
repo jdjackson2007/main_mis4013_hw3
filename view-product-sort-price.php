150e9059badbf5dbbd168e7e0932aadae1ddd0f7
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-20 13:41:29 -0600

    Update view-product-sort-price.php

--- view-product-sort-price.php
+++ view-product-sort-price.php
@@ -1,12 +1,14 @@
-<h1>Product Sort Price</h1>
+<h1>Product Grouped by Price</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
       <th>Product ID</th>
         <th>Category ID</th>
+      <th>Vendor Product ID</th>
         <th>Product Name</th>
       <th>Product Description</th>
+      <th>Product Quanity</th>
         <th>Product Price</th>
       </tr>
      
@@ -18,8 +20,10 @@
   <tr>
    <td><?php echo $product ['Product_ID'];?></td>
    <td><?php echo $product ['Category_ID'];?></td>
+   <td><?php echo $product ['VendorProduct_ID'];?></td>
    <td><?php echo $product ['Product_Name'];?></td>
    <td><?php echo $product ['Product_Description'];?></td>
+   <td><?php echo $product ['Product_Quanity'];?></td>
    <td><?php echo $product ['Product_Price'];?></td>
   
     
