87f253e82855bd4b7a17928f0e9ae62537d11d91
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-20 13:13:14 -0600

    Create model-product-sort-price.php

---
+++ model-product-sort-price.php
@@ -0,0 +1,16 @@
+<?php
+function selectProduct($pprice) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Product_ID,Category_ID,Product_Name,Product_Description,Product_Price FROM `mis-4013_hw3`.product_table WHERE Product_Price=?");
+         $stmt->bind_param("i", $pprice);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
