b6d4b5d58863510ea6211fa97e4ecdf9023ed488
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-20 20:05:05 -0600

    Update model-product-within-category.php

--- model-product-within-category.php
+++ model-product-within-category.php
@@ -2,7 +2,7 @@
 function selectProductWithinCategory($cid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Product_ID,Category_ID,VendorProduct_ID,Product_Name,Product_Description,Product_Quanity,Product_Price FROM `mis-4013_hw3`.product_table WHERE Category_ID=?");
+        $stmt = $conn->prepare("SELECT Product_ID,Category_ID,Product_Name,Product_Description,Product_Quanity,Product_Price FROM `mis-4013_hw3`.product_table WHERE Category_ID=?");
          $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
